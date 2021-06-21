e517daecf0c68e20fdc619c82d86769631acd595
Lorenzo <61149733+TinacciL@users.noreply.github.com>
2021-06-21 13:41:52 +0200

    Update FromGraphToXYZ.py

--- FromGraphToXYZ.py
+++ FromGraphToXYZ.py
@@ -1,16 +1,68 @@
-from mol_lib import MolFromGraphs, mol_graph_image
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import networkx as nx
-name = 'C9H5' + '+'
-atoms = ['H','H','H','H','H','C','C','C','C','C','C','C','C','C']
-bonds = [1,1,1,1,1,1,3,1,3,1,3,1,2]
-edges = [[0,5],[1,5],[2,5],[3,13],[4,13],[5,6],[6,7],[7,8],[8,9],[9,10],[10,11],[11,12],[12,13]]
+from ase import io, neighborlist, atoms
+
+#Function that encode the molecules from graph object to XYZ format
+def MolFromGraphs(G):
+    # Function based by https://stackoverflow.com/questions/51195392/smiles-from-graph
+	'''
+	Function that takes as input the networkx graph (each node must have an atom property H,N,C,O etc) and return the mol (rdkit) object
+	the function dont discriminate between different type of bond, it care only about the connectivity.
+	'''
+	adjacency_matrix = nx.convert_matrix.to_numpy_matrix(G,weight='bond').tolist()
+	node_list = []
+	for i,node in enumerate(G):
+		node_list.append(G.nodes[i]['atom'])
+	#Create empty editable mol object
+	mol = Chem.RWMol()
+	#Add atoms to mol and keep track of index
+	node_to_idx = {}
+	for i in range(len(node_list)):
+		a = Chem.Atom(node_list[i])
+		molIdx = mol.AddAtom(a)
+		node_to_idx[i] = molIdx
+	#Add bonds between adjacent atoms
+	for ix, row in enumerate(adjacency_matrix):
+		for iy, bond in enumerate(row):
+			#Only traverse half the matrix
+			if iy >= ix:
+				break
+			#Add relevant bond type (there are many more of these)
+			if bond == 0:
+				continue
+			elif bond == 1:
+				bond_type = Chem.rdchem.BondType.SINGLE
+				mol.AddBond(node_to_idx[ix], node_to_idx[iy], bond_type)
+			elif bond == 2:
+			    bond_type = Chem.rdchem.BondType.DOUBLE
+			    mol.AddBond(node_to_idx[ix], node_to_idx[iy], bond_type)
+			elif bond == 3:
+			    bond_type = Chem.rdchem.BondType.TRIPLE
+			    mol.AddBond(node_to_idx[ix], node_to_idx[iy], bond_type)
+	mol.UpdatePropertyCache(strict=False)
+    #Add the possibility in N bearing species cations to have valence 4 for the N center
+	for at in mol.GetAtoms():
+		if at.GetAtomicNum() == 7 and at.GetExplicitValence()==4 and at.GetFormalCharge()==0:
+			at.SetFormalCharge(1)     
+	Chem.SanitizeMol(mol)
+	return mol
+
+#INPUT - Change the above line in order to save the computed .xyz in the choose path    
+path = '/Users/aaa/Documents/FromGraphToXYZ/'
+#INPUT - Name of the molecules for the .xyz file
+name = 'CH4'
+#INPUT - Ordinated list of atoms in the choose molecule
+atoms = ['H','H','H','H','C']
+#INPUT - Ordinated list of bond in the choose molecule (1 = sigma, 2 = pi, 3 = 2 * pi)
+bonds = [1,1,1,1]
+#INPUT - Ordinated order of Connettivity between list of atom
+edges = [[0,4],[1,4],[2,4],[3,4]]
+
+#Upload all the info in graph object
 connectivity = []
 for i,item in enumerate(edges):
     connectivity.append((edges[i][0],edges[i][1], {'bond': bonds[i]}))
-#connectivity = [(edges[0][0],edges[0][1], {'bond': bonds[0]}),(edges[1][0],edges[1][1], {'bond': bonds[1]}),(edges[2][0],edges[2][1], {'bond': bonds[2]}),(edges[3][0],edges[3][1], {'bond': bonds[3]}), (edges[4][0],edges[4][1], {'bond': bonds[4]}), (edges[5][0],edges[5][1], {'bond': bonds[5]})]
-path = '/Users/tinaccil/Documents/GitHub/tmp/ions/miss_cations/'
 n_dim = len(atoms)
 G = nx.Graph()
 for i,atom in enumerate(atoms):
@@ -18,11 +70,13 @@ for i,atom in enumerate(atoms):
     tmp_attr = {'atom': atom}
     G.nodes[i].update(tmp_attr.copy())
 G.add_edges_from(connectivity)
+
+#Encode the molecules from graph object to XYZ format
 tmp_mol = MolFromGraphs(G)
-#mol_graph_image(G)
-#rint(Chem.MolToSmiles(tmp_mol))
+#Read the molecules in RdKit
 AllChem.EmbedMolecule(tmp_mol)
+#Optimized the guessed structure with UFF in RdKit
 AllChem.UFFOptimizeMolecule(tmp_mol)
+#Save the .XYZ file of the optimized structure
 with open(path + name + '.xyz', "w+") as file_mol:
     file_mol.write(Chem.MolToXYZBlock(tmp_mol))
-os.system('ase gui ' + path + name + '.xyz')
